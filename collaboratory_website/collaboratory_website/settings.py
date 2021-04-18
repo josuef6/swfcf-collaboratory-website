7b6329c5ed8e122993573014da40fd8cf2bb1047
Josue <josuef@umich.edu>
2021-04-18 10:39:30 -0400

    Update settings.py

--- collaboratory_website/collaboratory_website/settings.py
+++ collaboratory_website/collaboratory_website/settings.py
@@ -68,10 +68,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware'
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'collaboratory_website.urls'
 
 TEMPLATES = [
